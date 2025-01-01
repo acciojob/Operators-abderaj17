fe074e68ebff5b727a35f3cff9e5f267da57a7ee
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-01-01 17:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const firstInp = document.querySelector('#num1')
+const secondInp = document.querySelector('#num2');
+const btn = document.querySelector('#calculate');
+const resultF = document.querySelector('#result');
+const operator = document.querySelector('#operator')
+
+btn.addEventListener('click', function(){
+	let num1 = parseFloat(firstInp.value);
+	let num2 = parseFloat(secondInp.value);
+	let operation = operator.value;
+	let result;
+
+	switch(operation){
+		case '+':
+			result = num1+num2;
+			break;
+		case '-':
+			result= num1-num2;
+		    break;
+		case '*':
+			result = num1*num2;
+			break;
+		case '/':
+			result= num1/num2;
+	}
+	resultF.textContent = result;
+})
